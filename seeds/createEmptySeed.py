80809a182493fb40641746030596bd28625e26ea
davekch <dave-koch@web.de>
2018-06-26 13:45:48 +0200

    temporarily ignore seeds/

--- seeds/createEmptySeed.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-WIDTH, HEIGHT = int(sys.argv[1]), int(sys.argv[2])
-
-with open("newEmptySeed.txt", "w") as f:
-    for j in range(HEIGHT):
-        for i in range(WIDTH):
-            f.write("0 ")
-        f.write("\n")
