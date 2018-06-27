dc63dadf8c138a20653a5a4aea8fde43bf4919d2
davekch <dave-koch@web.de>
2018-06-28 00:25:22 +0200

    make cythonized library for conway-specific functions

---
+++ cycon.pyx
@@ -0,0 +1,53 @@
+import random
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import animation
+import argparse
+import copy
+
+def randomPopulate(width,height, density):
+    alive = np.zeros((width,height))
+    for i in range(width):
+        for j in range(height):
+            alive[i,j] = 1 if random.uniform(0.,1.)<density else 0
+    return alive
+
+def populate(seed):
+    alive = np.loadtxt(seed, dtype=np.int)
+    return alive
+
+cdef int countLiveNeighbours(alive, int i, int j):
+    cdef int liveNeighbours=0
+    cdef int di
+    cdef int dj
+    for di in range(-1,2):
+        for dj in range(-1,2):
+            if not (di==0 and dj==0):
+                try:
+                    if alive[i+di, j+dj]==1:
+                        liveNeighbours+=1
+                except IndexError:
+                    pass
+    return liveNeighbours
+
+# takes 2d-numpyarray and returns one
+cdef tick( alive ):
+    cdef int width, height
+    width, height = alive.shape
+    updated = copy.deepcopy(alive)
+    cdef int i
+    cdef int j
+    for i in range(width):
+        for j in range(height):
+            if alive[i,j]==1:
+                # if less than two live neighbours, cell dies
+                if countLiveNeighbours(alive, i,j) < 2:
+                    updated[i,j] = 0
+                # if more than 3 live neighbours, cell dies
+                elif countLiveNeighbours(alive, i,j) > 3:
+                    updated[i,j] = 0
+            else:
+                # if 3 live neighbours, cell comes to life
+                if countLiveNeighbours(alive, i,j) == 3:
+                    updated[i,j] = 1
+    return updated
